3dbdc2f033839e8c7e91033b0301d8defbe1bc16
EmilioRivFa <emiliorivfa@gmail.com>
2025-06-16 12:38:33 -0600

    correccion

--- app.py
+++ app.py
@@ -174,5 +174,7 @@ def tsp_tabu():
         'distancia_total': costo
     })
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
